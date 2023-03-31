edc2e4d5a43d408627297554b1c921088e6fa4f7
sekine104 <sekine104@outlook.jp>
2023-03-31 20:09:38 +0900

    update: メソッドを簡単に呼び出せるように変更

--- avgle/__init__.py
+++ avgle/__init__.py
@@ -10,5 +10,82 @@ class Avgle(Client):
         self.videos = Videos(self)
         self.info = Informations(self)
 
+    def get_videos(self, page=0, limit=50):
+        """
+        get videos by page.
+
+        :param page (optional): integer / default 0
+        :param limit (optional): interger 1~250 / default 50
+        """
+        return self.videos.get_videos(page, limit)
+
+
+    def search_videos(self, query, page=0, limit=50,
+                    o=OrderBy.LATEST, t=Date.FOREVER, tp=None, c=None):
+        """
+        search videos by query.
+
+        :param query (required): string / not empty.
+        :param page (optional): integer / default 0
+        :param limit (optional): interger 1~250 / default 50
+        :param o (optional): string / default mr
+        :: bw: (Last viewed)
+        :: mr: (Latest)
+        :: mv: (Most viewed)
+        :: tr: (Top rated)
+        :: tf: (Most favoured)
+        :: lg: (Longest)
+        :param t (optional): string / default a
+        :: t: (1 day)
+        :: w: (1 week)
+        :: m: (1 month)
+        :: a: (forever)
+        :param tp (optional): string / public or private
+        :param c (optional): interger(CHID of a valid video category)
+        """
+        return self.videos.search_videos(query, page, limit, o, t, tp, c)
+
+
+    def search_JAVs(self, query, page=0, limit=50,
+                    o=OrderBy.LATEST, t=Date.FOREVER, tp=None, c=None):
+        """
+        search Japanese AV by query.
+
+        :param query (required): string / not empty.
+        :param page (optional): integer / default 0
+        :param limit (optional): interger 1~250 / default 50
+        :param o (optional): string / default mr
+        :: bw: (Last viewed)
+        :: mr: (Latest)
+        :: mv: (Most viewed)
+        :: tr: (Top rated)
+        :: tf: (Most favoured)
+        :: lg: (Longest)
+        :param t (optional): string / default a
+        :: t: (1 day)
+        :: w: (1 week)
+        :: m: (1 month)
+        :: a: (forever)
+        :param tp (optional): string / public or private
+        :param c (optional): interger(CHID of a valid video category)
+        """
+        return self.videos.search_JAVs(query, page, limit, o, t, tp, c)
+
+
+    def get_video_by_id(self, video_id: int|str):
+        """
+        get video by id.
+
+        :param video_id (required): interger.
+        """
+        return self.videos.get_video_by_id(video_id)
+
+    def get_video_categories(self):
+        return self.info.get_video_categories()
+
+    def get_video_collections(self, page=1):
+        return self.info.get_video_collections(page)
+
+
 
 __all__ = ["Avgle", "AvgleError", "OrderBy", "Date"]
--- example.py
+++ example.py
@@ -4,4 +4,4 @@ client = avgle.Avgle()
 
 print(client.videos.get_videos(limit=1)["response"]["videos"][0]['title'])
 print(client.info.get_video_categories()['response']['categories'][0])
-print(client.videos.search_JAVs("AV", o=avgle.OrderBy.LATEST, t=avgle.Date.MONTH)['response']['videos'][0]['title'])
+print(client.search_JAVs("AV", o=avgle.OrderBy.LATEST, t=avgle.Date.MONTH)['response']['videos'][0]['title'])
